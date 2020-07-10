8af2db94d96cf21ffd59336273e5d9f75452b4c9
Tony Xu <tonxu@linkedin.com>
2020-07-10 10:52:45 -0700

    Fix noopener issue
    See https://mathiasbynens.github.io/rel-noopener

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -9,7 +9,7 @@ class ProfileCard extends Component {
       <div className="profile">
         <div className="profile-container">
           <img src={this.props.pictureURL} alt="" height="200px" width="200px"/>
-          <h1><a href={this.props.profileURL} target="_blank">{this.props.firstName} {this.props.lastName}</a></h1>
+          <h1><a href={this.props.profileURL} target="_blank" rel="noopener noreferrer">{this.props.firstName} {this.props.lastName}</a></h1>
           <h2>{this.props.headline}</h2>
         </div>
       </div>
